814dfbb0c589ccbb6e3a60dc685bfa4ab49c851a
James Capps <jcapps@scottrade.com>
2017-01-23 16:19:42 -0600

    Add Contact Us page layout and styles

--- src/components/contact/ContactUsPage.js
+++ src/components/contact/ContactUsPage.js
@@ -4,6 +4,33 @@ class ContactUsPage extends React.Component {
     render() {
         return (
             <div id="contact-page">
+                <h3><b>Contact Us</b></h3>
+                <p>We encourage you to contact us with any inquiries or to set up a consultation.</p>
+                <br/>
+                <h4>By Phone</h4>
+                <p>(314) 384-6204</p>
+                <br/>
+                <h4>By Mail</h4>
+                <p>
+                    P.O. Box 410474<br/>
+                    St. Louis, MO 63141
+                </p>
+                <br/>
+                <h4>By E-mail</h4>
+                <table>
+                    <tbody>
+                        <tr><td className="email-label">Name</td></tr>
+                        <tr><td><input maxLength="25"/></td></tr>
+                        <tr><td><span>First</span></td></tr>
+                        <tr><td><input maxLength="25"/></td></tr>
+                        <tr><td><span>Last</span></td></tr>
+                        <tr><td className="email-label">E-mail Address</td></tr>
+                        <tr><td><input maxLength="50"/></td></tr>
+                        <tr><td className="email-label">Message</td></tr>
+                        <tr><td><textarea rows="8" cols="40"></textarea></td></tr>
+                        <tr><td><button>Send</button></td></tr>
+                    </tbody>
+                </table>
             </div>
         );
     }
