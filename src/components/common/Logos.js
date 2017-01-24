7b2e416440438298fcd160c2bb5cb1b1af6a70e2
James Capps <jcapps@scottrade.com>
2017-01-24 11:01:55 -0600

    Use alternate logos when browsing with IE or Edge

--- src/components/common/Logos.js
+++ src/components/common/Logos.js
@@ -2,14 +2,29 @@ import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
 
 const Logos = () => {
+    const isInternetExplorerOrEdge = () => {
+        return navigator.appName == 'Microsoft Internet Explorer' 
+            || (navigator.appName == "Netscape" && navigator.appVersion.indexOf('Edge') > -1);
+    };
+
+    let logo = require("../../images/SynvisusLogo.png");
+    let motto = require("../../images/Motto.png");
+
+    if (isInternetExplorerOrEdge) {
+        logo = require("../../images/SynvisusLogoIE.png");
+        motto = require("../../images/MottoIE.png");
+    }
+
     return (
         <div id="logos-container">
             <Link to="/">
-                <img id="logo" src={require("../../images/SynvisusLogo.png")} alt="Logo"/>
+                <img id="logo" src={logo} alt="Logo"/>
             </Link>
-            <img id="motto" src={require("../../images/Motto.png")} alt="Motto"/>
+            <img id="motto" src={motto} alt="Motto"/>
         </div>
     );
 };
+
+
 
 export default Logos;
