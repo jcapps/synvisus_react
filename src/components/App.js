14876be48bc83b21ac367052bcabd076f0737596
jcapps <jamesmcapps@gmail.com>
2017-02-09 09:03:20 -0600

    Remove loading prop passed to Header

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     render() {
         return (
             <div id="container">
-                <Header loading={this.props.loading}/>
+                <Header/>
                 <div id="page-content">
                     {this.props.children}
                 </div>
