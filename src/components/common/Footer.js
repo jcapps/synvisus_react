1fb55489d5b0046b82c109ac0a4320dd9cdbcbd0
jcapps <jamesmcapps@gmail.com>
2017-02-09 09:49:21 -0600

    Add Terms of Use and Privacy Policy

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,12 +1,19 @@
-import React, {PropTypes} from 'react';
-import { Link, IndexLink } from 'react-router';
+import React from 'react';
+
+const openTerms = () => {
+    window.open('/terms', null, 'height=500, width=500');
+};
+
+const openPrivacy = () => {
+    window.open('/privacy', null, 'height=500, width=500');
+};
 
 const Footer = () => {
     return (
         <footer>
-            <span>Terms of Use</span>
+            <span><a onClick={openTerms}>Terms of Use</a></span>
             <span> | </span>
-            <span>Privacy Policy</span>
+            <span><a onClick={openPrivacy}>Privacy Policy</a></span>
 
             <p>
                 &copy; 2014 Synvisus LLC | P.O. Box 410474, St. Louis, MO 63141 | (314) 384-6204
--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,19 @@ import AboutPage from './components/about/AboutPage';
 import WhatWeDoPage from './components/whatwedo/WhatWeDoPage';
 import DemosPage from './components/demos/DemosPage';
 import ContactUsPage from './components/contact/ContactUsPage';
+import TermsPage from './components/terms/TermsPage';
+import PrivacyPage from './components/privacy/PrivacyPage';
 
 export default (
-    <Route path="/" component={App}>
-        <IndexRoute component={HomePage} />
-        <Route path="about" component={AboutPage} />
-        <Route path="whatwedo" component={WhatWeDoPage} />
-        <Route path="demos" component={DemosPage} />
-        <Route path="contact" component={ContactUsPage} />
-    </Route>
+    <div>
+        <Route path="/" component={App}>
+            <IndexRoute component={HomePage} />
+            <Route path="about" component={AboutPage} />
+            <Route path="whatwedo" component={WhatWeDoPage} />
+            <Route path="demos" component={DemosPage} />
+            <Route path="contact" component={ContactUsPage} />
+        </Route>
+        <Route path="/terms" component={TermsPage} />
+        <Route path="/privacy" component={PrivacyPage} />
+    </div>
 );
