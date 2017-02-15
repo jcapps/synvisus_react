3f990d7cf8cc22bdfd775b8cbee9ecc9567e4f26
jcapps <jamesmcapps@gmail.com>
2017-02-15 17:06:45 -0600

    Use private variables for addresses

--- tools/mailHelper.js
+++ tools/mailHelper.js
@@ -1,4 +1,5 @@
 import * as MAILGUN_INFO from './private/mailgunInfo';
+import * as MAILING_DETAILS from './private/mailingDetails';
 import mailgun from 'mailgun-js';
 const mg = mailgun({apiKey: MAILGUN_INFO.KEY, domain: MAILGUN_INFO.DOMAIN});
 
@@ -6,8 +7,8 @@ const sendMail = (email, callback) => {
     const subjectHeader = "Message from Synvisus: " 
         + email.firstName + " " + email.lastName + " <" + email.emailAddress + ">";
     let testEmail = {
-        from: 'noreply.sba@gmail.com',
-        to: ['jamesmcapps@gmail.com'],
+        from: MAILING_DETAILS.SENDER_ADDRESS,
+        to: MAILING_DETAILS.RECIPIENTS,
         subject: subjectHeader,
         text: email.message
     };
