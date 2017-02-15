b464cd360fd6b5eeb5e3f695ccfcdb19ce5b075b
jcapps <jamesmcapps@gmail.com>
2017-02-14 23:31:11 -0600

    Update port in config files

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -8,7 +8,7 @@ import sendMail from './mailHelper';
 
 /* eslint-disable no-console */
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const compiler = webpack(config);
 
